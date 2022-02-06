e6151300bcbedb475cb6eaad44643e95a9a926d9
rania-gamal-integrant <rania.gamal@integrant.com>
2022-02-06 10:25:25 +0200

    added plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const htmlPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = (env)=>{
     return {
         mode: env,
         output: {
             filename: 'bundle.js'
-        }
+        },
+        plugins:[new htmlPlugin(), new webpack.ProgressPlugin()]
     }
 }
