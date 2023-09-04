bd38f46488b6c79611716f9b7d1e8e8f63b31451
Rania Gamal <rania.gamal@vodafone.com>
2023-09-04 23:03:39 +0300

    output management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import _ from "lodash";
 import "./styles.css";
 import Icon from "./icon.png";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
-
+  const btn = document.createElement("button");
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(["Hello", "webpack"], " ");
   element.classList.add("hello");
@@ -14,6 +15,10 @@ function component() {
   myIcon.src = Icon;
 
   element.appendChild(myIcon);
+  btn.innerHTML = "Click me!";
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
